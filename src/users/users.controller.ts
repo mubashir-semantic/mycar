38e80b89df4354c82b56f04720d46f661b91622d
mubashir-semantic <mubashir@semanticsol.com>
2026-08-24 12:44:25 +0500

    feat: setup NestJS architecture, database connection, and user entity

---
+++ src/users/users.controller.ts
@@ -0,0 +1,55 @@
+import {
+  Body,
+  Controller,
+  Post,
+  Get,
+  Param,
+  Delete,
+  Query,
+  Patch,
+  NotFoundException,
+} from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
+import { UpdateUserDto } from './dtos/update-user.dto';
+
+@Controller('auth')
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+  @Post('/signup')
+  createUser(@Body() body: CreateUserDto) {
+    return this.usersService.createUser(body.email, body.password);
+  }
+
+  @Get('/:id')
+  async findUser(@Param('id') id: string) {
+    const user = await this.usersService.findOne(parseInt(id));
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return user;
+  }
+
+  @Get()
+  findAllUsers(@Query('email') email: string) {
+    return this.usersService.find(email);
+  }
+
+  @Delete('/:id')
+  async deleteUser(@Param('id') id: string) {
+    const user = await this.usersService.findOne(parseInt(id));
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return this.usersService.remove(parseInt(id));
+  }
+
+  @Patch('/:id')
+  async updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    const user = await this.usersService.findOne(parseInt(id));
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return this.usersService.update(parseInt(id), body);
+  }
+}
