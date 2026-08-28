b097a49e7b844cf3a33791cdc8c8769b3215a795
mubashir-semantic <mubashir@semanticsol.com>
2026-08-28 11:35:13 +0500

    Fixes

--- data-source.ts
+++ data-source.ts
@@ -3,7 +3,7 @@ import { DataSource } from 'typeorm';
 export const appDataSource = new DataSource({
   type: 'better-sqlite3',
   database: 'db.sqlite',
-  synchronize: false, // Migrations ke liye isay false rakhte hain
+  synchronize: false,
   entities: ['src/**/*.entity.ts'],
-  migrations: ['src/migrations/*.ts'], // Migrations is folder mein save hongi
+  migrations: ['src/migrations/*.ts'],
 });
